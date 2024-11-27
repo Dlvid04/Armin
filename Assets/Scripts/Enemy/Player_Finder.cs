d502c706e947e291411c688a86588c5c06dbd72f
Dudin47 <Dustin.stennermx.de>
2024-11-27 20:12:38 +0100

    Version 0.56.7

--- Assets/Scripts/Enemy/Player_Finder.cs
+++ Assets/Scripts/Enemy/Player_Finder.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class Player_Finder : MonoBehaviour
@@ -13,6 +15,7 @@ public class Player_Finder : MonoBehaviour
     public Transform Player;
     public Transform EnemyTransform;
     public Animator EnemyAnimator;
+    public PlayableDirector PlayableDirector;
 
     public float gravity = -9.8f;
 
@@ -26,26 +29,23 @@ public class Player_Finder : MonoBehaviour
 
     public string Scene;
 
-    bool IsMoving = false;
-
     void Start(){
-        EnemyNav.gameObject.SetActive(false);
+        EnemyTransform.gameObject.SetActive(true);
     }
     // Update is called once per frame
     void Update()
     {
+        Entrance();
+        if (PlayableDirector.state != PlayState.Playing) {
+            EnemyNav.SetDestination(Player.position);
             Door_Open_Close();
             Falling();
-            EnemyNav.SetDestination(Player.position);
             Check_If_In_Range();
-            if (!IsMoving)
-            {
-                Check_If_Moving(EnemyTransform.position);
-            }
+        }
     }
 
     public void CallMethod() {
-        GameObject.Find("Door_Home_Entrance").transform.GetChild(0).GetChild(1).GetComponentInParent<DoorController>().SendMessage("ToggleDoor");
+        GameObject.Find("Door_Home_Entrance").transform.GetChild(1).GetComponentInParent<DoorController>().SendMessage("ToggleDoor");
     }
 
     public void Door_Open_Close() {
@@ -54,13 +54,12 @@ public class Player_Finder : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, See_Range))
         {
-            if (hit.collider.gameObject.CompareTag("Door"))
+            if (hit.collider.gameObject.name == "Rotator")
             {
-                Debug.Log(hit.collider.gameObject.name);
                 if (!hit.collider.gameObject.GetComponentInParent<DoorController>().isOpen && hit.collider.gameObject.name == "Rotator") {
                     hit.collider.gameObject.GetComponentInParent<DoorController>().SendMessage("ToggleDoor"); // Hier greift der Gegner auf T�ren zu !!! Zu Oft !!!
                 }
-                if (hit.collider.gameObject.transform.parent.parent.name == "Door_Home_Entrance")
+                if (hit.collider.gameObject.transform.parent.name == "Door_Home_Entrance")
                 {
                     Invoke("CallMethod", 1f);
                 }
@@ -90,17 +89,11 @@ public class Player_Finder : MonoBehaviour
         }
     }
 
-    public void Check_If_Moving(Vector3 LastPosition) {
-        float distanceMoved = Vector3.Distance(transform.position, LastPosition);
-
-        if (distanceMoved > 0.01f)
-        {
+    public void Entrance() {
+        Vector3 currentPosition = EnemyTransform.position;
+        if (PlayableDirector.state != PlayState.Playing) {
             EnemyAnimator.SetBool("IsWalking", true);
-            EnemyAnimator.SetBool("IsStanding", false);
-            IsMoving = true;
-        }else
-            EnemyAnimator.SetBool("IsWalking", false);
-            EnemyAnimator.SetBool("IsStanding", true);
-            IsMoving = false;
+            EnemyTransform.position = currentPosition;
+        }
     }
 }
