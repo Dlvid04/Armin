35bb4f1fd49d496e7c6782e772b95f8ed94bd045
Dudin47 <Dustin.stennermx.de>
2024-12-04 19:08:34 +0100

    Version 0.56.9.5

--- Assets/Scripts/Riddles/Home_Dark_Riddle.cs
+++ Assets/Scripts/Riddles/Home_Dark_Riddle.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Home_Dark_Riddle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Riddle1;
+    public GameObject Riddle2;
+    public GameObject Riddle3;
+
+    public float rayLength = 4f;
+    [SerializeField] GameObject Enemy;
+
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, rayLength)) {
+            if (hit.collider.gameObject.CompareTag("Interactable")) {
+                if (Input.GetKeyDown("e")) {
+                    if (FindFirstObjectByType<Inventory>().keyId == "key1" && hit.collider.gameObject.name == "Cube_Dark") {
+                        GameObject.Find("Rust_Key_Dark (2)").GetComponent<Animator>().SetBool("Erscheinen", true);
+                    }
+                    if (FindFirstObjectByType<Inventory>().keyId == "key_Dark_1" && hit.collider.gameObject.name == "Cube_Dark (1)") {
+                        GameObject.Find("Rust_Key_Dark (3)").GetComponent<Animator>().SetBool("Erscheinen", true);
+                    }
+                    if (FindFirstObjectByType<Inventory>().keyId == "key_Dark_2" && hit.collider.gameObject.name == "Cube_Dark (2)") {
+                        GameObject.Find("Rust_Key_Dark (4)").GetComponent<Animator>().SetBool("Erscheinen", true);
+                    }
+                    if (FindFirstObjectByType<Inventory>().keyId == "key_Dark_3" && hit.collider.gameObject.name == "Padlock") {
+                        GameObject.Find("Padlock").SetActive(false);
+                        Enemy.SetActive(true);
+                        FindFirstObjectByType<Player_Finder>().Scene = "Dark_Start";
+                    }
+                }
+            }
+        }
     }
 }
